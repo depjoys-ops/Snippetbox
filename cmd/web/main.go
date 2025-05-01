51c62e8626261b369b218f1bd30b34cc3d331e9d
Oleh Kuzmych <depjoys@gmail.com>
2025-05-01 13:41:46 +0000

    added DI, error handling, isolate routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,29 +2,32 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
+
+type application struct {
+    logger *slog.Logger
+}
 
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 	
-	mux := http.NewServeMux()
-	
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level: slog.LevelInfo,
+	}))
+	app := &application{
+        logger: logger,
+    }
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-    mux.HandleFunc("GET /snippet/create", snippetCreate)
-    mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	logger.Info("starting server", "addr", *addr)
 
-
-	log.Printf("starting server on %s", *addr)
-
-	err := http.ListenAndServe(*addr, mux)
-    log.Fatal(err)
+	err := http.ListenAndServe(*addr, app.routes())
+    logger.Error(err.Error())
+	os.Exit(1)
 
 }
