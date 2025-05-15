e1e4e898801aba140f21cfc0772ca8f256d0639f
Oleh <depjoys@gmail.com>
2025-05-15 12:23:43 +0300

    tests

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,53 +2,56 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
-	"io/fs"
 
 	"github.com/depjoys-ops/Snippetbox/internal/models"
 	"github.com/depjoys-ops/Snippetbox/ui"
 )
 
 type templateData struct {
-	CurrentYear int
-    Snippet 	models.Snippet
-	Snippets 	[]models.Snippet
-	Form 		any
-	Flash       string
+	CurrentYear     int
+	Snippet         models.Snippet
+	Snippets        []models.Snippet
+	Form            any
+	Flash           string
 	IsAuthenticated bool
-	CSRFToken	string
+	CSRFToken       string
 }
 
-func humanDate(t time.Time) string {	
-    return t.Format("02 Jan 2006 at 15:04")
+func humanDate(t time.Time) string {
+	if t.IsZero() {
+		return ""
+	}
+	return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
 var functions = template.FuncMap{
-    "humanDate": humanDate,
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 	pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl")
 	if err != nil {
-        return nil, err
-    }
+		return nil, err
+	}
 
 	for _, page := range pages {
 		name := filepath.Base(page)
-		
+
 		patterns := []string{
-            "html/base.tmpl",
-            "html/partials/*.tmpl",
-            page,
-        }
+			"html/base.tmpl",
+			"html/partials/*.tmpl",
+			page,
+		}
 
 		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
-        if err != nil {
-            return nil, err
-        }
-		
+		if err != nil {
+			return nil, err
+		}
+
 		cache[name] = ts
 	}
 
