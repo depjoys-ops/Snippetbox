c4d30e6309705a24bfb80b1baed46215832ad696
Oleh Kuzmych <depjoys@gmail.com>
2025-05-19 18:48:13 +0000

    bug fix

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -27,6 +27,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
+		return
 	}
 
 	snippet, err := app.snippets.Get(id)
@@ -36,6 +37,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		} else {
 			app.serverError(w, r, err)
 		}
+		return
 	}
 
 	data := app.newTemplateData(r)
